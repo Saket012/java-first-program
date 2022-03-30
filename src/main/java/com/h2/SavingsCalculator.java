ad0a243d1046be84aadc72647b7399c9a550abdd
saket <saket0124@gmail.com>
2022-03-30 23:29:49 +0530

    saving calc

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -7,10 +7,10 @@ public class SavingsCalculator {
 private float[] debits;
 private float[] credits;
 
-public SavingsCalculator(float[] debits , float[] credits){
-    this.debits=debits;
-    this.credits=credits;
-}
+    public SavingsCalculator(float[] credits, float[] debits) {
+        this.credits = credits;
+        this.debits = debits;
+    }
 private float sumOfCredits(){
     float sum=0.0f;
     for (int i=0; i<credits.length;i++){
